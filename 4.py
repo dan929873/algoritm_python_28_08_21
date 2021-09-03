a862a0b0d2264d5962a82a11337800a1e5bde3d1
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-09-03 10:25:01 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -5,11 +5,15 @@
 #     случайный символ.
 
 import random
-import string
 
-x = float(input("\tx1 = "))
-y = float(input("\ty1 = "))
-
+x = int(input("\tinteger 1 = "))
+y = int(input("\tinteger 2 = "))
 print(random.randint(x, y))
+
+x = float(input("\tfloat 1 = "))
+y = float(input("\tfloat 2 = "))
 print(random.uniform(x, y))
-print(''.join(random.choice(string.ascii_letters)))
+
+x = input("\tchar 1 = ")
+y = input("\tchar 2 = ")
+print(chr(random.randrange(ord(x), ord(y))))
