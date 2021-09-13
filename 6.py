8562c385359b4b8d97ad502a2cbe5070bde842d9
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-09-13 22:43:02 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,6 +1,22 @@
-# 6. Пользователь вводит номер буквы в алфавите. Определить, какая это бу
+# 6. В программе генерируется случайное целое число от 0 до 100. Пользователь должен его отгадать не более чем за 10 попыток. 
+# После каждой неудачной попытки должно сообщаться больше или меньше введенное пользователем число, чем то, что загадано. 
+# Если за 10 попыток число не отгадано, то вывести загаданное число.
 
-from idna import unichr
 
-x = int(input("char number = "))
-print(str(unichr(x+65)))
+
+from random import random
+n = round(random() * 100)
+i = 1
+print("Computer thought of a number. Guess it. You have 10 attempts")
+while i <= 10:
+    u = int(input(str(i) + 'attempt: '))
+    if u > n:
+        print('many')
+    elif u < n:
+        print('little')
+    else:
+        print(f'You guessed it right on {i} dth try')
+        break
+    i += 1
+else:
+    print(f'You have exhausted 10 attempts. It was envisioned{n}')
