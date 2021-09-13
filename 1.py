86b2ca48dee0795a7e2ac285653005af4bfb5b8e
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-09-13 22:03:37 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,7 +5,34 @@
 # Также сообщать пользователю о невозможности деления на ноль, если он ввел 0 в качестве делителя.
 
 
-while(1):
-    x1 = input("number 1 = ")
-    s = input("simbol = ")
-    x2 = input("number 2 = ")
+def my_calculation(x1, x2, s):
+    if s == '-':
+        return x1 - x2
+    if s == '+':
+        return x1 + x2
+    if s == '*':
+        return x1 * x2
+    if s == '/':
+        return x1 / x2
+
+
+s = 'n'
+simbol_ligal = [ '+', '-', '*', '/']
+while(s != '0'):
+    try:
+        x1 = float(input("number 1 = "))
+        s = input("simbol = ")
+        x2 = float(input("number 2 = "))
+        if s in simbol_ligal:
+            if s == '/' and x2 == '0':
+                print("not correct to divide by zero")
+                continue
+            print(f"result: {my_calculation(x1, x2, s)}")
+        elif s != '0':
+            print("you not correct simbol, for exit '0'")
+            continue
+    except ValueError:
+        print("Please input number")
+        continue
+else:
+    print("Bye my user")
